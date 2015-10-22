5c8b1d6c434d54b444a2df81a756496a9fcd83f7
Alexander Maringele <nyaya@maringele.at>
2015-10-22 19:57:57 +0200

    simplified Yices API tests (demos)

--- TentativeTests/SatTryTests.swift
+++ TentativeTests/SatTryTests.swift
@@ -37,41 +37,45 @@ class SatTryTests: XCTestCase {
         
         yices_assert_formula(ctx, p)
         XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
+        
+        let mdl = yices_get_model(ctx, 1);
+        defer {
+            yices_free_model(mdl)
+        }
+        
+        var bvalue : Int32 = -1
+        
+        for (formula,expected):(term_t,Int32) in [(p,1),(np,0), (wahr,1), (falsch,0), (nichtwahr,0)
+            ] {
+            let name = String(term:formula)
+            let code = yices_get_bool_value(mdl, formula, &bvalue)
+            XCTAssertEqual(0,code,name!)
+            XCTAssertEqual(expected,bvalue,name!)
+            let mval = yices_formula_true_in_model(mdl, formula)
+            
+             print("\(name) : \(bvalue) \(mval)")
+                
+        }
+        
         yices_assert_formula(ctx, np)
-        XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
-
-        
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, wahr)
-        XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, falsch)
-        XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, nichtwahr)
         XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
     }
     
     func testEquations() {
         let ctx = yices_new_context(nil)
         defer {  yices_free_context(ctx) }
-    
-//        let tau = yices_new_uninterpreted_type()
-        let tau = yices_int_type()
-
+        
+        // types
+        let tau = yices_new_uninterpreted_type()
         let truth = yices_bool_type()
+        let unary = yices_function_type1(tau, tau)
         
         let c = yices_new_uninterpreted_term(tau)   // 'constant' with unknown value of unknown type
         yices_set_term_name(c, "c")
-    
-        let unary = yices_function_type1(tau, tau)
         let f = yices_new_uninterpreted_term(unary)
         yices_set_term_name(f, "f")
         let g = yices_new_uninterpreted_term(unary)
-        yices_set_term_name(f, "g")
+        yices_set_term_name(g, "g")
         
         let fc = yices_application1(f, c)
         let gc = yices_application1(g, c)
@@ -90,23 +94,20 @@ class SatTryTests: XCTestCase {
         
         print(String(model:mdl)!)
         
-        var val = yices_get_bool_value(mdl, eq, pvalue)
-        print("eq:\(val) \(global_value)")
+        var bvalue : Int32 = -1
         
-        val = yices_get_bool_value(mdl, ne, pvalue)
-        print("ne:\(val) \(global_value)")
-        
-        val = yices_get_bool_value(mdl, neq, pvalue)
-        print("neq:\(val) \(global_value)")
-        
-        yices_assert_formula(ctx, neq)
-        XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
+        for (formula,expected):(term_t,Int32) in [(eq,0), (ne,1), (neq,1)] {
+            let name = String(term:formula)
+            let code = yices_get_bool_value(mdl, formula, &bvalue)
+            XCTAssertEqual(0,code,name!)
+            XCTAssertEqual(expected,bvalue,name!)
+            let mval = yices_formula_true_in_model(mdl, formula)
+            
+            print("\(name) : \(bvalue) \(mval)")
+        }
         
         yices_assert_formula(ctx, eq)
         XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
-        
-        
-        
     }
     
     func testPredicates() {
@@ -138,7 +139,6 @@ class SatTryTests: XCTestCase {
         yices_assert_formula(ctx, npfc)
         XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
         
-        
         let mdl = yices_get_model(ctx, 1);
         defer {
             yices_free_model(mdl)
@@ -146,35 +146,20 @@ class SatTryTests: XCTestCase {
         
         print(String(model:mdl)!)
         
-        let pfc_val = yices_get_bool_value(mdl, pfc, pvalue)
+        var bvalue : Int32 = -1
         
-        print("pfc:\(pfc_val) \(global_value)")
-        
-        let npfc_val = yices_get_bool_value(mdl, npfc, pvalue)
-        
-        print("npfc:\(npfc_val) \(global_value)")
-        
-        let pfc_in_model = yices_formula_true_in_model(mdl,pfc)
-        let npfc_in_model = yices_formula_true_in_model(mdl,npfc)
-        
-        print("pfc in model:\(pfc_in_model)")
-        print("npfc in model:\(npfc_in_model)")
-
+        for (formula,expected):(term_t,Int32) in [(pfc,0), (npfc,1), (wahr,1), (falsch,0)] {
+            let name = String(term:formula)
+            let code = yices_get_bool_value(mdl, formula, &bvalue)
+            XCTAssertEqual(0,code,name!)
+            XCTAssertEqual(expected,bvalue,name!)
+            let mval = yices_formula_true_in_model(mdl, formula)
+            
+            print("\(name) : \(bvalue) \(mval)")
+        }
       
         yices_assert_formula(ctx, pfc)
         XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, wahr)
-        XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, falsch)
-        XCTAssertTrue(STATUS_UNSAT == yices_check_context(ctx, nil))
-        
-        yices_reset_context(ctx)
-        yices_assert_formula(ctx, yices_implies(falsch, wahr))
-        XCTAssertTrue(STATUS_SAT == yices_check_context(ctx, nil))
     }
     
     func testSymbols() {

a9f5207cb4e903d1839f59592963ed178d0b2c7b
Alexander Maringele <alm@ydwlt-e-16-54.uibk.ac.at>
2015-12-23 11:17:45 +0100

    Update TptpPath. Comments, access.

--- ParseC/String+TptpPath.swift
+++ ParseC/String+TptpPath.swift
@@ -3,10 +3,11 @@
 
 import Foundation
 
-/// Absolute path to a tptp file (problem, axiom).
+/// Absolute path to a tptp file, i.e. a problem or an axiom.
 ///
-///     '/Users/Shared/TPTP/Problems/PUZ/PUZ001-1.p'
-///     '/Users/Shared/TPTP/Axioms/PUZ001-0.ax'
+/// /Users/Shared/TPTP/Problems/PUZ/PUZ001-1.p
+///
+/// /Users/Shared/TPTP/Axioms/PUZ001-0.ax
 public typealias TptpPath = String
 
 /// A tptp path to a file contains three components.
@@ -22,13 +23,15 @@ typealias TptpPathComponents = (root:TptpPath,local:TptpPath,last:TptpPath)
 
 public extension TptpPath {
     /// Splits a tptp path to its components.
+    ///
+    /// "/Users/Shared/TPTP/Problems/PUZ/PUZ001-1.p" -> (root:"/Users/Shared/TPTP/",local:"Problems/PUZ/",last:"PUZ001-1.p")
     private func tptpPathComponents () -> TptpPathComponents {
         var rootPath = [TptpPath]()
         var localPath = [TptpPath]()
         
         var isRootPathComponent = true
-        for pathcomponent in ((self as NSString).stringByDeletingLastPathComponent as NSString).pathComponents {
-            switch pathcomponent {
+        for pathComponent in ((self as NSString).stringByDeletingLastPathComponent as NSString).pathComponents {
+            switch pathComponent {
             case "Axioms", "Problems":
                 isRootPathComponent = false;
             default:
@@ -36,23 +39,24 @@ public extension TptpPath {
             }
             
             if isRootPathComponent {
-                rootPath.append(pathcomponent)
+                rootPath.append(pathComponent)
             } else {
-                localPath.append(pathcomponent)
+                localPath.append(pathComponent)
             }
         }
         
         return (NSString.pathWithComponents(rootPath), NSString.pathWithComponents(localPath), (self as NSString).lastPathComponent)
     }
     
-    /// It's assumed that 'file' shares the same root directory as 'self'.
-    public func tptpPathTo(file:TptpPath) -> TptpPath {
+    /// TPTP problem files can include axioms, i.e. the local path to an axiom file.
+    /// It is assumed that `file` share the same root path as `self`.
+    private func tptpPathTo(file:TptpPath) -> TptpPath {
         let (root,_,_) = self.tptpPathComponents()
         let (_,directory,name) = file.tptpPathComponents()
         
-        let path = directory.isEmpty ? (root as NSString).stringByAppendingPathComponent(name) : ((root as NSString).stringByAppendingPathComponent(directory) as NSString).stringByAppendingPathComponent(name)
+        guard !directory.isEmpty else { return (root as NSString).stringByAppendingPathComponent(name) }
         
-        return path
+        return ((root as NSString).stringByAppendingPathComponent(directory) as NSString).stringByAppendingPathComponent(name)
     }
     
     /// Find path to tptp include file (usually an axiom).

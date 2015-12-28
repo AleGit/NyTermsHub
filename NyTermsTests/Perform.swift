d8e43eb73304bdefbf8bcc8cb2243a2448134c66
Alexander Maringele <alm@ydWLT-E-16-54.uibk.ac.at>
2015-12-28 17:01:14 +0100

    string tries test

---
+++ NyTermsTests/Perform.swift
@@ -0,0 +1,35 @@
+//
+//  Perform.swift
+//  NyTerms
+//
+//  Created by Alexander Maringele on 28.12.15.
+//  Copyright © 2015 Alexander Maringele. All rights reserved.
+//
+
+import XCTest
+
+class Perform: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- NyTermsTests/StringTrieTests.swift
+++ NyTermsTests/StringTrieTests.swift
@@ -53,10 +53,74 @@ class StringTrieTests: XCTestCase {
         let unknown = Array(goodbye.characters)
         
         XCTAssertNil(trie[unknown])
+    }
+    
+    func myprint(ts:[(String,CFAbsoluteTime)]) {
+        
+        for (index,pair) in ts[1..<ts.count].enumerate() {
+            let duration = pair.1 - ts[index].1
+            print("\(pair.0)\t\(duration)")
+        }
+        
+    }
+    
+    func testStringTriePerformance() {
+        let prefix = "x1 a __ xx 432"
+        var words = [prefix,"a","b","c","d","e"]
+
+        var ts = [("Start",CFAbsoluteTimeGetCurrent())]
+        let limit = 1_000_000
+        repeat   {
+            outer: for first in words {
+                for second in words {
+                    
+                    words.append(first+second)
+                    
+                    guard words.count < limit else { break outer }
+                }
+            }
+        }
+        while words.count < limit
+        
+        
+        
+        XCTAssertEqual(limit, words.count)
+        ts.append(("WordsBuild", CFAbsoluteTimeGetCurrent()))
+        
+        // print(words)
+        // ts.append(("WordsPrint", CFAbsoluteTimeGetCurrent()))
         
         
         
         
+        let trie = buildStringTrie(words)
+        ts.append(("TrieBuild", CFAbsoluteTimeGetCurrent()))
+        
+        let path = Array((prefix+"abc").characters)
+        guard let trieResult = trie[path]?.payload else {
+            XCTFail()
+            return
+        }
+        ts.append(("TrieSearch", CFAbsoluteTimeGetCurrent()))
+        
+        
+        let mapResult = words.filter {
+            $0.hasPrefix(prefix+"abc")
+        }
+        ts.append(("ArrayFilter", CFAbsoluteTimeGetCurrent()))
+        myprint(ts)
+        
+        
+        
+        XCTAssertEqual(limit,mapResult.count)
+        
+        
+        
+        
+        
+        
+        
+
         
         
     }

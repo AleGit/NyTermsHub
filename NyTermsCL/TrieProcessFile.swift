477670d854a71f14cb80a6d03d5a85360e0b5882
Alexander Maringele <nyaya@maringele.at>
2016-02-27 07:24:59 +0100

    Update trie process file

---
+++ NyTermsCL/TrieProcessFile.swift
@@ -0,0 +1,9 @@
+//
+//  TrieProcessFile.swift
+//  NyTerms
+//
+//  Created by Alexander Maringele on 27.02.16.
+//  Copyright © 2016 Alexander Maringele. All rights reserved.
+//
+
+import Foundation

9c241e381fbf0ede9f03759b07c910d4c986e26c
Alexander Maringele <alm@ydWLT-U-5-208.uibk.ac.at>
2016-04-20 13:28:19 +0200

    Add Yices.LPO + Test

---
+++ NyTermsTests/Yices+LPOTests.swift
@@ -0,0 +1,73 @@
+//
+//  Yices+LPOTests.swift
+//  NyTerms
+//
+//  Created by Alexander Maringele on 20.04.16.
+//  Copyright © 2016 Alexander Maringele. All rights reserved.
+//
+
+import XCTest
+@testable import NyTerms
+
+class Yices_LPOTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        yices_init()
+    }
+    
+    override func tearDown() {
+        yices_exit()
+        super.tearDown()
+    }
+    
+    func testE435() {
+        let ctx = yices_new_context(nil)
+        defer { yices_free_context(ctx) }
+        var lpo = Yices.LPO()
+        
+        let trs : [(TptpNode,TptpNode)] = [
+            ("add(o,Y)", "Y"),
+            ("add(s(X),Y)", "s(add(X,Y))"),
+            ("mul(o,Y)", "o"),
+            ("mul(s(X),Y)", "add(mul(X,Y),Y)"),
+            ]
+        
+        for (s,t) in trs {
+            let c = lpo.leftRightCondition(s, t)
+            
+            yices_assert_formula(ctx, c)
+            print(s,">",t,"\t ⬄ \t",String(term:c)!)
+        }
+        
+        XCTAssertEqual(STATUS_SAT,yices_check_context(ctx, nil))
+        
+        let mdl = yices_get_model(ctx, 1)
+        defer {
+            yices_free_model(mdl)
+        }
+        
+        let precedences = lpo.precedences.map {
+            (t) -> (String,Int32) in
+            let f = String(term:t)!
+            var p : Int32 = -1
+            
+            if yices_get_int32_value(mdl, t, &p) == 0 {
+                return(f,p)
+            } else {
+                print(f, "yices_get_int32_value(mdl,\(f),&val) failed.")
+                return(f,-1)
+            }
+            
+            }.sort {
+                $0.1 > $1.1
+        }
+        
+        print (precedences)
+        
+
+        
+        
+    }
+
+}
--- Yices/Yices+Order.swift
+++ Yices/Yices+Order.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-
-
 extension Node {
     func isFnX(f:Symbol, _ X:Symbol) -> Bool {
         guard let nodes = self.nodes else {
@@ -22,8 +20,6 @@ extension Node {
         return nodes.first!.isFnX(f, X)
     }
 }
-
-
 
 extension Yices {
     struct KBO {
@@ -170,6 +166,130 @@ extension Yices {
             }
             
             return Yices.and (w0gt0, Yices.and(conditions), Yices.and(unariesConditions))
+        }
+    }
+}
+
+extension Yices {
+    struct LPO {
+        let pPrefix = "p⏑"
+        
+        var symbols = [String : (precedence:term_t, arity: Int)]()
+        
+        var precedences : [term_t] {
+            return symbols.map { $0.1.precedence }
+        }
+        
+        mutating func register(symbol:String, arity:Int) -> term_t {
+            guard let (p,a) = symbols[symbol] else {
+                let precedence = Yices.constant("\(pPrefix)\(symbol)", term_tau:Yices.int_tau)
+                symbols[symbol] = (precedence,arity)
+                return precedence
+            }
+            
+            assert(a == arity, "\(symbol) must not be variadic: \(a) ≠ \(arity)")
+            
+            return p
+            
+        }
+        
+        mutating func leftRightCondition<N:Node>(s:N, _ t:N) -> term_t {
+            guard s.nodes != nil else {
+                // variables must not be on the left hand side
+                return Yices.bot()
+            }
+            
+            // s = f(s_1,...,s_n) >_lpo t if one of three holds
+            
+            return Yices.or ([case1(s,t), case2(s,t), case3(s,t)])
+        }
+        
+        
+        mutating private func case1<N:Node>(s:N, _ t:N) -> term_t {
+            guard s.symbol == t.symbol else {
+                // case 1 is not applicable if symbols are different
+                return Yices.bot()
+            }
+            
+            guard let snodes = s.nodes, let tnodes = t.nodes
+                where snodes.count == tnodes.count else {
+                    // this must not happen because symbols are equal, 
+                    // hence t cannot be a variable or 
+                    // a function symbol with different arity
+                    assert(false,"\(#function)(\(s),\(t)) - mismatch in type or arity of root symbols.")
+                return Yices.bot()
+            }
+            
+            var conditions = [term_t]()
+            var lastWereEqual = true
+            for (sj,tj) in zip(snodes,tnodes) {
+                if lastWereEqual && sj==tj {
+                    // currents are Equal
+                    continue
+                }
+                else if lastWereEqual {
+                    // sj != tj
+                    let si_gt_ti = leftRightCondition(sj,tj)
+                    conditions.append(si_gt_ti)
+                    lastWereEqual = false
+                }
+                else {
+                    let s_gt_tj = leftRightCondition(s,tj)
+                    conditions.append(s_gt_tj)
+                }
+            }
+            
+            if lastWereEqual {
+                // all were equal, hence s == t, hence not (s > t)
+                return Yices.bot()
+            }
+            else {
+                return Yices.and(conditions)
+            }
+        }
+        
+        mutating private func case2<N:Node>(s:N, _ t:N) -> term_t {
+            guard let tnodes = t.nodes where s.symbol != t.symbol else {
+                // case 2 is not applicable if t is a variable
+                // case 2 is not applicable if symbols are the same
+                return Yices.bot()
+            }
+            
+            let f = s.symbolString()
+            let g = t.symbolString()
+            
+            guard let snodes = s.nodes else {
+                assert(false,"\(#function)(\(s),_) - must not be called with a variable as first argument.")
+                return Yices.bot()
+            }
+            
+            let pf = register(f, arity: snodes.count)
+            let pg = register(g, arity: tnodes.count)
+            
+            let pf_gt_pg = Yices.gt(pf,pg)
+            
+            let conditions = [pf_gt_pg] + tnodes.map {
+                leftRightCondition(s,$0)
+            }
+            
+            return Yices.and(conditions)
+        }
+        
+        mutating private func case3<N:Node>(s:N, _ t:N) -> term_t {
+            guard let snodes = s.nodes else {
+                assert(false,"\(#function)(\(s),_) - must not be called with a variable as first argument.")
+                return Yices.bot()
+            }
+            
+            let conditions = snodes.map {
+                (si) -> term_t in
+                if si == t { return Yices.top() }
+                else {
+                    return leftRightCondition(si,t)
+                }
+            }
+            
+            return Yices.or(conditions)
         }
     }
 }

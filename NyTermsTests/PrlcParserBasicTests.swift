faff15a052e0232f29b0124dccd498c7facde80c
Alexander Maringele <alm@ydWLT-U-1-234.uibk.ac.at>
2016-05-10 12:35:59 +0200

    Update Prlc basic tests

--- NyTermsTests/PrlcParserBasicTests.swift
+++ NyTermsTests/PrlcParserBasicTests.swift
@@ -43,71 +43,79 @@ class PrlcParserBasicTests: XCTestCase {
     }
     
     func testParsing() {
-        for (name, timeLimit, expectedTreeSize, _) in [
-            ("PUZ001-1",0.003, 106, 12),
-            ("PUZ051-1", 1.0 ,110 ,0),
+        for (name, timeLimit, expectedTreeSize, expectedFormulaCount) in [
+            ("PUZ001-1",    0.002   ,106    , 12), // 12
+            ("PUZ051-1",    0.007   ,110    ,  3),  // 43 (1 inlc)
             
-            ("SYN000-2",1.0,145, 0),
+            ("SYN000-2",    0.003   ,145    , 18),          // 19 (1 incl)
             
-            ("HWV001-1",0.019, 413, 0),
-            ("HWV002-1",0.003, 436, 0),
+            ("HWV001-1",    0.002   ,413        , 42),      // 47 (1 incl)
+            ("HWV002-1",    0.002   ,436        , 51),      // 51
             
-            ("HWV003-1",0.019, 474, 0),
-            ("HWV003-2",0.1, 372, 0),
-            ("HWV003-3",0.019, 407, 0),
+            ("HWV003-1",    0.014   ,474, 42),  // 47 (1 incl)
+            ("HWV003-2",    0.011     ,372        , 36),      // 36
+            ("HWV003-3",    0.002   ,407        , 61),      // 61
             
-            ("HWV004-1",0.019, 441, 0),
+            ("HWV004-1",    0.010   ,441        , 36),      // 41 (1 incl)
             
-            ("HWV005-1",0.1, 65, 0),
-            ("HWV005-2",0.5, 53, 0),
+            ("HWV005-1",    0.004   ,65         , 10),      // 42 (2 incl)
+            ("HWV005-2",    0.004   ,53         , 10),      // 42 (2 incl)
             
-            ("HWV006-1",0.01, 123, 0),
+            ("HWV006-1",    0.008    ,123        , 16),      // 58 (3 incl)
             
-            ("HWV007-1",0.1, 115, 0),
-            ("HWV007-2",0.005, 95, 0),
+            ("HWV007-1",    0.007    ,115        , 14),      // 56 (3 incl)
+            ("HWV007-2",    0.006   ,95         , 14),      // 62 (3 incl)
             
-            ("HWV008-1.001",0.01, 181, 0),
-            ("HWV008-1.002",0.1, 288, 0),
-            ("HWV008-2.001",0.01, 134, 0),
-            ("HWV008-2.002",0.01, 210, 0),
+            ("HWV008-1.001",0.008    ,181        , 18),      // 60 (3 incl)
+            ("HWV008-1.002",0.010    ,288        , 26),      // 68 (3 incl)
+            ("HWV008-2.001",0.007    ,134        , 18),      // 66 (3 incl)
+            ("HWV008-2.002",0.009    ,210        , 26),      // 74 (3 incl)
             
-            ("HWV009-1",0.01, 24, 0),
+            ("HWV009-1",    0.007    ,24         , 3),       // 92 (1 incl)
             
-            ("HWV052-1.005.004",0.01,1812, 0),
-            ("HWV052-1.007.004",0.01,3874, 0),
+            ("HWV052-1.005.004",0.002,1812       , 52),      // 52
+            ("HWV052-1.007.004",0.005,3874       , 86),      // 86
             
-            ("HWV056-1",1.9,1970598, 0),
-            ("HWV074-1",0.4,241278, 0),
-            ("HWV076-1",0.1,20830, 0),
-            ("HWV078-1",0.1,10330, 0),
-            ("HWV080-1",0.2,121264, 0),
-            ("HWV091-1",2.9,1652994, 0),
-            ("HWV092-1",15.3,9377099, 0),
-            ("HWV093-1",0.1,30972, 0),
-            ("HWV098-1",3.9,2378326, 0),
-            ("HWV106-1",4.9,3381734, 0),
-            ("HWV107-1",1.0,490794, 0),
-            ("HWV117-1",0.9,228613, 0),
-            ("HWV124-1",9.9,7098296, 0),
-            ("HWV134-1",45.6, 29_953_326, 2_332_428)
+            ("HWV056-1",    1.2     ,1970598    , 9661),    // 9661
+            ("HWV074-1",    0.152     ,241278     , 2581),    // 2581
+            ("HWV076-1",    0.031     ,20830      , 1424),    // 1424
+            ("HWV078-1",    0.012     ,10330      , 449),     // 449
+            ("HWV080-1",    0.103     ,121264     , 1852),    // 1852
+            ("HWV091-1",    1.99     ,1652994    , 125811),  // 125811
+            ("HWV092-1",   11.1     ,9377099    , 696691),  // 696691
+            ("HWV093-1",    0.051     ,30972      , 2652),    // 2652
+            ("HWV098-1",    3.0     ,2378326    , 202745),  // 202745
+            ("HWV106-1",    4.2     ,3381734    , 287949),  // 287949
+            ("HWV107-1",    0.7     ,490794     , 40269),   // 40269
+            ("HWV117-1",    0.4     ,228613     , 17625),   // 17625
+            ("HWV124-1",    8.9     ,7098296    , 555949),  // 555949
+            ("HWV134-1",36.9, 29_953_326, 2_332_428)
             ] {
                 guard let path = name.p else {
                     XCTFail("Did not find path for \(name)")
                     continue
                 }
                 
-                print(name,path)
                 
+                print(name,path)
                 let (pair,parseTime) = measure {
                     
                     prlcParse(path)
                 }
                 
-                XCTAssertTrue(parseTime < max(1.0,timeLimit),"\(name) \(parseTime.prettyTimeIntervalDescription)")
+                guard let table = pair.1 where pair.0 == 0 else {
+                    XCTFail("\(name) \(path) was not parsed correctly: \(pair.0)")
+                    continue
+                }
                 
-                let code = pair.0
-                XCTAssertEqual(0, code, "\(code)")
-                XCTAssertEqual(expectedTreeSize, pair.1?.treeStoreSize, "\(expectedTreeSize) \(pair.1?.treeStoreSize)")
+                let c0 = table.treeStoreSize
+                let c1 = table.tptpSequence.count { _ in true }
+                
+                print(name,":",parseTime.prettyTimeIntervalDescription,timeLimit.prettyTimeIntervalDescription,"•",c0,expectedTreeSize,"-",c1,expectedFormulaCount)
+                
+                XCTAssertTrue(parseTime < 2*timeLimit, "\(name) \(parseTime.prettyTimeIntervalDescription)")
+                XCTAssertEqual(expectedTreeSize, c0)
+                XCTAssertEqual(expectedFormulaCount, c1)
                 
                 
                 

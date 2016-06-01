e0e16f276247f2bdcd6942fe8dd3f3b448d91dc1
Alexander Maringele <aXm@ydWLT-E-16-252.uibk.ac.at>
2016-06-01 13:16:55 +0200

    Update general yices tests

--- NyTermsTests/YicesTestCase.swift
+++ NyTermsTests/YicesTestCase.swift
@@ -23,4 +23,31 @@ class YicesTestCase : XCTestCase {
         super.tearDown()
     }
     
+    func testNegations() {
+        
+        let p = "p(f(X,Y))" as TptpNode
+        let np = "~p(f(A,B))" as TptpNode
+        
+        XCTAssertEqual(p.description, "p(f(X,Y))")
+        XCTAssertEqual(np.description, "~(p(f(A,B)))")
+        
+        let yp = Yices.clause(p).yicesClause    // p
+        let ynp = Yices.clause(np).yicesClause   // ~p
+        
+        let ypt = yices_type_of_term(yp)
+        let ynpt = yices_type_of_term(ynp)
+        
+        print(yp,  ypt, String(tau:ypt)!)
+        print(ynp, ynpt, String(tau:ynpt)!)
+        
+        let nyp = yices_not(yp) // ~p
+        let nynp = yices_not(ynp) // ~~p
+        
+        XCTAssertEqual(yp, nynp)    // p = ~~p
+        XCTAssertEqual(ynp, nyp)    // ~p = ~p
+        XCTAssertEqual(yices_not(nynp), nyp)
+        
+        
+    }
+    
 }

507e97d36a0d04d35432d7d0759101f6bcd6a341
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-06-30 13:24:53 +0200

    Add smt api tests

--- NyTermsTests/SmtApiInfoTests.swift
+++ NyTermsTests/SmtApiInfoTests.swift
@@ -8,19 +8,37 @@ import NyTerms
 /// Basic Usage
 ///
 /// [yices.csl.sri.com/doc/basic-usage.html](http://yices.csl.sri.com/doc/basic-usage.html)
-class YicesBasicTests: XCTestCase {
+class SmtApiInfoTests: XCTestCase {
 
     /// Check installed yices version.
     func testYicesInfo() {
+
         let info = Yices.info
+        
+        XCTAssertTrue(info.containsString("Yices"))
+        XCTAssertTrue(info.containsString("2.4."))
         
         for previous in ["2.4.0", "2.4.1"] {
             XCTAssertNil(info.rangeOfString(previous))
         }
         
-        XCTAssertTrue(info.containsString("Yices"))
-        XCTAssertTrue(info.containsString("2.4."))
-        
         XCTAssertEqual("Yices 2.4.2 (x86_64-apple-darwin15.2.0,release,2015-12-11)", info)
+    }
+    
+    func testZ3Info() {
+        
+        let info = Z3.info
+        
+        
+        XCTAssertTrue(info.containsString("Z3 "))
+        XCTAssertTrue(info.containsString(" 4.4."))
+        
+        for previous in ["4.4.1.0"] {
+            XCTAssertNil(info.rangeOfString(previous))
+        }
+        
+        XCTAssertEqual("Z3 4.4.2.0", info)
+
+        
     }
 }
